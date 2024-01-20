22b4f1f26bbe1702ab03e68f14a9ee49225937c3
Narmin-G-M <narmin.g.mammadli@gmail.com>
2024-01-20 21:20:56 +0400

    Create Update DOM method

--- DOM Array Methods/script.js
+++ DOM Array Methods/script.js
@@ -1,35 +1,62 @@
-const main = document.getElementById('main');
-const addUserBtn = document.getElementById('add-user');
-const doubleBtn = document.getElementById('double');
-const showMillionairesBtn = document.getElementById('show-millionaires');
-const sortBtn = document.getElementById('sort');
-const calculateWealthBtn = document.getElementById('calculate-wealth');
+const main = document.getElementById("main")
+const addUserBtn = document.getElementById("add-user")
+const doubleBtn = document.getElementById("double")
+const showMillionairesBtn = document.getElementById("show-millionaires")
+const sortBtn = document.getElementById("sort")
+const calculateWealthBtn = document.getElementById("calculate-wealth")
 
-let data = [];
+let data = []
 
-getRandomUser();
-getRandomUser();
-getRandomUser();
-getRandomUser();
+getRandomUser()
+getRandomUser()
+getRandomUser()
+getRandomUser()
 
 // Fetch random user and add money
 
 async function getRandomUser() {
-  const res = await fetch('https://randomuser.me/api');
-  const data = await res.json();
+  const res = await fetch("https://randomuser.me/api")
+  const data = await res.json()
 
-  const user = data.results[0];
+  const user = data.results[0]
 
   const newUser = {
     name: `${user.name.first} ${user.name.last}`,
     money: Math.floor(Math.random() * 1000000),
-  };
+  }
 
-  addUser(newUser);
+  addUser(newUser)
 }
 
 // Add new object to data array
 
 function addUser(obj) {
-  data.push(obj);
+  data.push(obj)
+
+  updateDOM()
 }
+
+// Update DOM
+
+function updateDOM(providedData = data) {
+  // clear main div
+
+  main.innerHTML = "<h2><strong>Person</strong> Wealth</h2>"
+
+  providedData.forEach((item) => {
+    const element = document.createElement("div")
+    element.classList.add("person")
+    element.innerHTML = `<strong>${item.name}</strong>${formatMoney(
+      item.money
+    )}`
+    main.appendChild(element)
+  })
+}
+
+// Format number as money - https://stackoverflow.com/questions/149055/how-to-format-numbers-as-currency-string
+function formatMoney(number) {
+  return "$" + number.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, "$&,")
+}
+
+// Event listeners
+addUserBtn.addEventListener("click", getRandomUser)
