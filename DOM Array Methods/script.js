6bc4eeb7234c1fb2c27a64b81914c9a843d4b45c
Narmin-G-M <narmin.g.mammadli@gmail.com>
2024-01-20 18:48:17 +0400

    Generate Random Users - Fetch w/ Async/Await

---
+++ DOM Array Methods/script.js
@@ -0,0 +1,35 @@
+const main = document.getElementById('main');
+const addUserBtn = document.getElementById('add-user');
+const doubleBtn = document.getElementById('double');
+const showMillionairesBtn = document.getElementById('show-millionaires');
+const sortBtn = document.getElementById('sort');
+const calculateWealthBtn = document.getElementById('calculate-wealth');
+
+let data = [];
+
+getRandomUser();
+getRandomUser();
+getRandomUser();
+getRandomUser();
+
+// Fetch random user and add money
+
+async function getRandomUser() {
+  const res = await fetch('https://randomuser.me/api');
+  const data = await res.json();
+
+  const user = data.results[0];
+
+  const newUser = {
+    name: `${user.name.first} ${user.name.last}`,
+    money: Math.floor(Math.random() * 1000000),
+  };
+
+  addUser(newUser);
+}
+
+// Add new object to data array
+
+function addUser(obj) {
+  data.push(obj);
+}
