32def424957a5d4b2737d3bd57c3214aba8caf50
Narmin-G-M <narmin.g.mammadli@gmail.com>
2024-01-22 07:18:43 +0400

    Calculate the total Wealth

--- DOM Array Methods/script.js
+++ DOM Array Methods/script.js
@@ -8,9 +8,6 @@ const calculateWealthBtn = document.getElementById("calculate-wealth")
 let data = []
 
 getRandomUser()
-getRandomUser()
-getRandomUser()
-getRandomUser()
 
 // Fetch random user and add money
 
@@ -36,6 +33,18 @@ function addUser(obj) {
   updateDOM()
 }
 
+// Calculate the total wealth
+
+function calculateWealth() {
+  const wealth = data.reduce((acc, user) => (acc += user.money), 0)
+
+  const wealthEl = document.createElement("div")
+  wealthEl.innerHTML = `<h3>Total Wealth: <strong> ${formatMoney(
+    wealth
+  )}</strong></h3>`
+  main.appendChild(wealthEl)
+}
+
 // Double Everyones Money
 
 function doubleMoney() {
@@ -50,6 +59,13 @@ function doubleMoney() {
 
 function sortByRichest() {
   data.sort((a, b) => b.money - a.money)
+
+  updateDOM()
+}
+
+// Filter only Millionaires
+function showMillionaires() {
+  data = data.filter((user) => user.money > 1000000)
 
   updateDOM()
 }
@@ -80,3 +96,5 @@ function formatMoney(number) {
 addUserBtn.addEventListener("click", getRandomUser)
 doubleBtn.addEventListener("click", doubleMoney)
 sortBtn.addEventListener("click", sortByRichest)
+showMillionairesBtn.addEventListener("click", showMillionaires)
+calculateWealthBtn.addEventListener("click", calculateWealth)
